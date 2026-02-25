fb685d32e4cb281886a6ccd81ce07fa108aa985a
srsbrna <srsbkusumadewi@gmail.com>
2026-02-25 11:51:29 +0700

    changes 1

---
+++ jobsheet 3/DosenDemo.java
@@ -0,0 +1,61 @@
+
+import java.util.Scanner;
+
+class dosen25 {
+    String kode;
+    String nama;
+    boolean jenisKelamin;
+    int usia;
+
+    public dosen25(String kode, String nama, boolean jenisKelamin, int usia) {
+        this.kode = kode;
+        this.nama = nama;
+        this.jenisKelamin = jenisKelamin;
+        this.usia = usia;
+    }
+
+    void tampil () {
+        System.out.println("Kode          : " + kode);
+        System.out.println("Nama          : " + nama);
+        System.out.println("Jenis kelamin : " + (jenisKelamin ? "pria" : "wanita"));
+        System.out.println("Usia          : " + usia);
+    }
+}
+
+public class DosenDemo {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan jumlah dosen: ");
+        int jumlah = sc.nextInt();
+        sc.nextLine();
+
+        dosen25[] arrayOfDosen = new dosen25[jumlah];
+
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("\nData Dosen ke-" + (i+1));
+
+            System.out.print("Kode : ");
+            String kode = sc.nextLine();
+
+            System.out.print("Nama : ");
+            String nama = sc.nextLine();
+
+            System.out.print("Jenis kelamin (true=Pria | false=wanita) : ");
+            boolean jenisKelamin = sc.nextBoolean();
+
+            System.out.print("Usia : ");
+            int usia = sc.nextInt();
+            sc.nextLine();
+
+            arrayOfDosen[i] = new dosen25(kode, nama, jenisKelamin, usia);
+        }
+
+        System.out.println("\n=== Data Dosen ===");
+        for (dosen25 d : arrayOfDosen) {
+            d.tampil();
+            System.out.println("-------------------------");    
+        }
+    }
+}
