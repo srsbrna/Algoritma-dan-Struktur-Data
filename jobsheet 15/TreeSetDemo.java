b34394863a42b9bfb666dceb0f73e44490a11b0a
srsbrna <srsbkusumadewi@gmail.com>
2026-06-03 09:16:55 +0700

    final

--- jobsheet 15/TreeSetDemo.java
+++ jobsheet 15/TreeSetDemo.java
@@ -12,5 +12,17 @@ public class TreeSetDemo {
         for (String temp : fruits) {
             System.out.println(temp);
         }
+
+        System.out.println("First: " + fruits.first());
+        System.out.println("Last: " + fruits.last());
+
+        fruits.remove("Jeruk");
+        System.out.println("Setelah remove " + fruits);
+
+        fruits.pollFirst();
+        System.out.println("Setelah poll first " + fruits);
+
+        fruits.pollLast();
+        System.out.println("Setelah poll last " + fruits);
     }
 }
