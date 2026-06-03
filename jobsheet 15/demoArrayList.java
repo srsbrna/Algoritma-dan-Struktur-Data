5d65c891ea5797dd8cad9c8a8e8b48fc723ef0fd
srsbrna <srsbkusumadewi@gmail.com>
2026-06-03 08:39:05 +0700

    pertanyaan 12

--- jobsheet 15/demoArrayList.java
+++ jobsheet 15/demoArrayList.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 public class demoArrayList {
     public static void main(String[] args) {
         ArrayList<customer> customers = new ArrayList<> (2);
+        ArrayList<customer> newCustomers = new ArrayList<> ();
+        newCustomers.add(new customer(201, "Della"));
+        newCustomers.add(new customer(202, "Victor"));
+        newCustomers.add(new customer(203, "Sarah"));
+
+        customers.addAll(newCustomers);
 
         customer customer1 = new customer(1, "Zakia");
         customer customer2 = new customer(5, "Budi");
@@ -16,9 +22,13 @@ public class demoArrayList {
 
         customers.add(2, new customer(100, "Rosa"));
 
-        for (customer cust : customers) {
-            System.out.println(cust.toString());
-        }
+        System.out.println(customers.indexOf(customer2));
+
+        customer customer = customers.get(1);
+        System.out.println(customer.name);
+        customer.name = "Budi Utomo";
+
+        System.out.println(customers);
 
     }
 }
